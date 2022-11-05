f89f9f54e56d0c4d213f143ab34ee209f6e70d23
hmcl2020 <444262771@qq.com>
2022-11-05 12:20:09 +0800

    NEW FILE

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout <<"input your passwd:"<<" ";
+    cin>>a;
+    cout <<"your passwd is:"<<" "<<a;
+    cout <<" ";
+    return 0;
+}
